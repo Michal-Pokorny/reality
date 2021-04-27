11c91a5b89b7a42fd7411fd2b186fba0ad140ac6
Michal-Pokorny <michal.pokorny@accenture.com>
2021-04-27 03:21:28 +0200

    job_seed test

--- jobs/job_seed.groovy
+++ jobs/job_seed.groovy
@@ -1,7 +1,4 @@
-pipelineJob('demo1') {	
-    pipeline {
-    agent any
-    
+pipelineJob('demo1') {	 
     parameters{
     	choiceParam(
   			'Type', ['prodej','pronajem','drazby','projekt']
@@ -15,45 +12,20 @@ pipelineJob('demo1') {
    		stringParam(
    			'Recipients', description: 'Sends E-mail with results to recipient(s)'
    		)
-    }    
-
-    stages {
-        stage('Build') {
-            steps {
-                git branch: 'main', url: 'https://github.com/Michal-Pokorny/reality'
+    } 
+    
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        github('https://github.com/Michal-Pokorny/reality')
+                    }
+                }
             }
+            scriptPath('jobs/sreality_param.groovy')
         }
-        stage('Prepare Docker') {
-            steps {
-                powershell '''docker-compose build'''
-            }
-        }
-        stage('Run Test') {
-            steps {
-                cleanWs(patterns: [[pattern: 'results/*.csv', type: 'INCLUDE']])
-                echo "Parameters: ${env.Type}, ${env.Realty}, ${env.Location}"
-                powershell "docker-compose run robot robot --include Param --variable auction_type:${env.Type} --variable realty:${env.Realty} --variable location:${env.Location} --variable size:${env.Size} execution/sreality.robot"
-                
-            }
-        }
-        stage('Send E-mail') {
-            when{
-                expression {env.Recipients}
-            }
-            steps {
-                echo "Sending results to ${env.Recipients}"
-                zip zipFile: 'results.zip', archive: false, dir: 'results'
-                emailext attachmentsPattern: 'results.zip', body: '''See attached results of SReality job''', subject: "SReality - Results", mimeType: 'text/html',to: env.Recipients
-                cleanWs(patterns: [[pattern: 'results.zip', type: 'INCLUDE']])
-            }
-        }
-        stage('Publish Results') {
-            steps {
-                robot archiveDirName: 'robot-plugin', outputPath: '', overwriteXAxisLabel: ''
-            }
-        }
-    }
-	}
+    }   
 }
 
 
